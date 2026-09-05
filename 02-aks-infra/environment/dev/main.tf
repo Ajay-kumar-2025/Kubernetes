bc33480abb38cdfd0d493d39e28a027c291b114e
Ajay-kumar-2025 <ajay.kr931@gmail.com>
2026-09-06 02:18:09 +0530

    updating pipeline.yml

--- 02-aks-infra/environment/dev/main.tf
+++ 02-aks-infra/environment/dev/main.tf
@@ -1,3 +1,13 @@
+import {
+  to = module.storage.azurerm_storage_account.example
+  id = "/subscriptions/xxx/resourceGroups/my-rg/providers/Microsoft.Storage/storageAccounts/mystorageaccount123"
+}
+
+import {
+  to = module.storage.azurerm_resource_group.example
+  id = "/subscriptions/<subscription-id>/resourceGroups/my-rg"
+}
+
 module "resource_group" {
   source          = "../../module/azurerm_resource_group"
   resource_groups = var.r_resource_groups
@@ -12,3 +22,4 @@ module "kubernetes_cluster" {
   source              = "../../module/azurerm_kubernetes_cluster"
   kubernetes_clusters = var.r_kubernetes_clusters
 }
+
