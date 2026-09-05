1ba79eba0512ba3e1a5a3868c1836f9cc2dfec09
Ajay-kumar-2025 <ajay.kr931@gmail.com>
2026-09-06 03:00:35 +0530

    updating terraform.tfvars

--- 02-aks-infra/environment/dev/main.tf
+++ 02-aks-infra/environment/dev/main.tf
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import {
-  to = module.storage.azurerm_storage_account.example
-  id = "/subscriptions/xxx/resourceGroups/my-rg/providers/Microsoft.Storage/storageAccounts/mystorageaccount123"
-}
-
-import {
-  to = module.storage.azurerm_resource_group.example
-  id = "/subscriptions/<subscription-id>/resourceGroups/my-rg"
-}
-
-module "resource_group" {
-  source          = "../../module/azurerm_resource_group"
-  resource_groups = var.r_resource_groups
-}
-module "storage_account" {
-  depends_on       = [module.resource_group]
-  source           = "../../module/azurerm_storage_account"
-  storage_accounts = var.r_storage_accounts
-}
-=======
 # module "resource_group" {
 #   source          = "../../module/azurerm_resource_group"
 #   resource_groups = var.r_resource_groups
@@ -28,13 +7,9 @@ module "storage_account" {
 #   source           = "../../module/azurerm_storage_account"
 #   storage_accounts = var.r_storage_accounts
 # }
->>>>>>> 2416da845eafe46a1ea3da63298aa49830e96c82
+
 module "kubernetes_cluster" {
-  depends_on          = [module.resource_group]
+ # depends_on          = [module.resource_group]
   source              = "../../module/azurerm_kubernetes_cluster"
   kubernetes_clusters = var.r_kubernetes_clusters
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 2416da845eafe46a1ea3da63298aa49830e96c82
