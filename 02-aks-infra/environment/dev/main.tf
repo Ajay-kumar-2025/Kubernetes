e33101ad00c7e5fa08b70c5f0dd41769ff8b8b3e
Ajay Kumar <ajay.kr931@gmail.com>
2026-09-06 02:08:12 +0530

    Comment out resource_group and storage_account modules

--- 02-aks-infra/environment/dev/main.tf
+++ 02-aks-infra/environment/dev/main.tf
@@ -1,12 +1,12 @@
-module "resource_group" {
-  source          = "../../module/azurerm_resource_group"
-  resource_groups = var.r_resource_groups
-}
-module "storage_account" {
-  depends_on       = [module.resource_group]
-  source           = "../../module/azurerm_storage_account"
-  storage_accounts = var.r_storage_accounts
-}
+# module "resource_group" {
+#   source          = "../../module/azurerm_resource_group"
+#   resource_groups = var.r_resource_groups
+# }
+# module "storage_account" {
+#   depends_on       = [module.resource_group]
+#   source           = "../../module/azurerm_storage_account"
+#   storage_accounts = var.r_storage_accounts
+# }
 module "kubernetes_cluster" {
   depends_on          = [module.resource_group]
   source              = "../../module/azurerm_kubernetes_cluster"
